af9e5da3686512abec718663d58acdf7aacb99ec
EphermeralCC <randomgameraccount@hotmail.com>
2015-11-16 16:59:54 -0500

    Added Spike Asset/Script + Player Movement
    Add trigger in player movement, added the spikes and script for spike.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 
-    public GameManager manager;
+    private GameManager manager;
 
     public float moveSpeed;
     public float rotationSpeed = 60f;
@@ -17,35 +17,42 @@ public class PlayerMovement : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
-        spawn = transform.position;
-        manager = manager.GetComponent<GameManager>();
+        spawn = this.transform.position;
+        manager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+
+        if (GetComponent<NetworkView>().isMine)
+        {
+            GetComponentInChildren<Camera>().enabled = true;
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-
-        if (GetComponent<Rigidbody>().velocity.magnitude < maxSpeed)
+        if (GetComponent<NetworkView>().isMine)
         {
-            GetComponent<Rigidbody>().AddRelativeForce(input * moveSpeed);
-        }
+            input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
 
-        if (Input.GetKey(KeyCode.Q))
-        {
-            transform.Rotate(0, -rotationSpeed * Time.deltaTime, 0.0f);
-        }
+            if (this.transform.GetComponent<Rigidbody>().velocity.magnitude < maxSpeed)
+            {
+                this.transform.GetComponent<Rigidbody>().AddRelativeForce(input * moveSpeed);
+            }
 
-        else if(Input.GetKey(KeyCode.E))
-        {
-            transform.Rotate(0, rotationSpeed * Time.deltaTime, 0.0f);
-        }
+            if (Input.GetKey(KeyCode.Q))
+            {
+                this.transform.Rotate(0, -rotationSpeed * Time.deltaTime, 0.0f);
+            }
 
-        if (transform.position.y < -5)
-        {
-            Death();
+            else if (Input.GetKey(KeyCode.E))
+            {
+                this.transform.Rotate(0, rotationSpeed * Time.deltaTime, 0.0f);
+            }
+
+            if (transform.position.y < -5)
+            {
+                Death();
+            }
         }
-        
 	
 	}
 
@@ -76,6 +83,11 @@ public class PlayerMovement : MonoBehaviour {
         {
             Death();
         }
+
+        if (other.transform.tag == "Enemy")
+        {
+            Death();
+        }
     }
 
     public void Death()
@@ -83,5 +95,20 @@ public class PlayerMovement : MonoBehaviour {
         Instantiate(deathParticles, transform.position, Quaternion.identity);
         transform.position = spawn;
         manager.deathCount += 1;
+    }
+
+    void OnSerializeNetworkView(BitStream stream, NetworkMessageInfo info)
+    {
+        Vector3 syncPosition = Vector3.zero;
+        if (stream.isWriting)
+        {
+            syncPosition = transform.GetComponent<Rigidbody>().position;
+            stream.Serialize(ref syncPosition);
+        }
+        else
+        {
+            stream.Serialize(ref syncPosition);
+            transform.GetComponent<Rigidbody>().position = syncPosition;
+        }
     }
 }
---
+++ Assets/Scripts/TrapScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrapScript : MonoBehaviour {
+
+    public float triggerTime;
+
+	// Use this for initialization
+	void Start () {
+
+        StartCoroutine(Go());
+	
+	}
+
+    IEnumerator Go()
+    {
+        while(true)
+        {
+            GetComponent<Animation>().Play();
+            yield return new WaitForSeconds(5f);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
